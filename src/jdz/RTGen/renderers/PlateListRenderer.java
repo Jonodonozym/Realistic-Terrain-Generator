f33f6f1e5f331bc2fc6fb1be31e72d8712227899
bakerjaid <jaidencolebaker@gmail.com>
2018-10-12 15:19:30 +1300

    Created PlateListRenderer

---
+++ src/jdz/RTGen/renderers/PlateListRenderer.java
@@ -0,0 +1,46 @@
+
+package jdz.RTGen.renderers;
+
+import static java.awt.Color.*;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.util.Arrays;
+import java.util.List;
+
+import jdz.RTGen.dataType.TectonicPlate;
+
+
+public class PlateListRenderer extends Renderer<List<TectonicPlate>> {
+	private List<Color> colors = Arrays.asList(WHITE, LIGHT_GRAY, GRAY, DARK_GRAY, BLACK, RED, PINK, ORANGE, YELLOW,
+			GREEN, MAGENTA, CYAN, BLUE);
+
+	@Override
+	public String getName() {
+		return "Tectonic Plates";
+	}
+
+	@Override
+	public void render(BufferedImage image, List<TectonicPlate> plates) {
+		int i = 0;
+
+		Graphics2D g = (Graphics2D) image.getGraphics();
+
+		for (TectonicPlate plate : plates) {
+			Color plateColor = colors.get(i++ % colors.size());
+			renderPlate(image, plate, plateColor);
+		}
+
+		g.dispose();
+	}
+
+	private void renderPlate(BufferedImage image, TectonicPlate plate, Color color) {
+		int rgb = color.getRGB();
+		for (int x = 0; x < plate.getMap().getWidth(); x++)
+			for (int y = 0; y < plate.getMap().getHeight(); y++)
+				if (plate.isInPlate(x, y))
+					image.setRGB(x, y, rgb);
+	}
+
+}
