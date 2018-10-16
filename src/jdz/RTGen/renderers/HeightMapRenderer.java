abfabe8dddbf8afb3ad965706683e0f41e162633
bakerjaid <jaidencolebaker@gmail.com>
2018-10-16 15:26:08 +1300

    Added Heightmap renderer

---
+++ src/jdz/RTGen/renderers/HeightMapRenderer.java
@@ -0,0 +1,43 @@
+
+package jdz.RTGen.renderers;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+import jdz.RTGen.dataType.Map;
+
+public class HeightMapRenderer extends Renderer<Map> {
+
+	@Override
+	public String getName() {
+		return "Height Map";
+	}
+
+	@Override
+	public void render(BufferedImage image, Map map) {
+		float minHeight = Float.MAX_VALUE;
+		float maxHeight = Float.MIN_VALUE;
+
+		for (int i = 0; i < map.size; i++) {
+			float height = map.cellHeight[i];
+			if (height < minHeight)
+				minHeight = height;
+			if (height > maxHeight)
+				maxHeight = height;
+		}
+
+		for (int x = 0; x < map.width; x++)
+			for (int y = 0; y < map.height; y++) {
+				float height = map.getHeight(x, y);
+				float ratio = (height - minHeight) / (maxHeight - minHeight);
+				image.setRGB(x, y, multiplyValue(image.getRGB(x, y), ratio));
+			}
+	}
+
+	private int multiplyValue(int rgb, float ratio) {
+		Color c = new Color(rgb);
+		float[] hsv = Color.RGBtoHSB(c.getRed(), c.getGreen(), c.getBlue(), null);
+		return Color.HSBtoRGB(hsv[0], hsv[1], hsv[2] * ratio);
+	}
+
+}
