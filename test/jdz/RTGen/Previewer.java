b759decaa248f689daa7edd6b1d9e05aaf0d7198
bakerjaid <jaidencolebaker@gmail.com>
2018-10-17 10:58:27 +1300

    Fixed scaling issue in previewer

--- test/jdz/RTGen/Previewer.java
+++ test/jdz/RTGen/Previewer.java
@@ -3,33 +3,61 @@ package jdz.RTGen;
 
 import java.awt.FlowLayout;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.Timer;
 
 import jdz.RTGen.dataType.Map;
 
-public abstract class Previewer {
+public abstract class Previewer implements ActionListener {
+	private static final int PREVIEW_WIDTH = 512, PREVIEW_HEIGHT = 256;
+
 	protected Map map;
 
-	public Previewer(int mapSize) {
-		map = new Map(mapSize * 2, mapSize);
-		
-		LoggerConfig.init();
-		
-		Image image = createPreview().getScaledInstance(1024, 512, Image.SCALE_SMOOTH);
+	private JFrame frame;
+	private ImageIcon icon;
 
-		JFrame frame = new JFrame();
+	public Previewer(int mapSize) {
+		this(mapSize, -1);
+	}
+
+	public Previewer(int mapSize, int updateMS) {
+		map = new Map(mapSize * 2, mapSize);
+
+		LoggerConfig.init();
+		init();
+
+		icon = new ImageIcon(getScaledImage());
+
+		if (updateMS > 0)
+			new Timer(updateMS, this).start();
+
+		frame = new JFrame();
 		frame.getContentPane().setLayout(new FlowLayout());
 
-		frame.getContentPane().add(new JLabel(new ImageIcon(image)));
+		frame.getContentPane().add(new JLabel(icon));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 
 		frame.setVisible(true);
 	}
 
+	protected void init() {}
+
+	private Image getScaledImage() {
+		return createPreview().getScaledInstance(PREVIEW_WIDTH, PREVIEW_HEIGHT, Image.SCALE_SMOOTH);
+	}
+
 	public abstract BufferedImage createPreview();
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		icon.setImage(getScaledImage());
+		frame.repaint();
+	}
 }
