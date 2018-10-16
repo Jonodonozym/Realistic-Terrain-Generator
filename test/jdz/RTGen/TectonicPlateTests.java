2f7a8c3d4e7c74ada9bc8c21e03a59c39629292b
bakerjaid <jaidencolebaker@gmail.com>
2018-10-17 10:47:52 +1300

    Added overlap and invert tests

--- test/jdz/RTGen/TectonicPlateTests.java
+++ test/jdz/RTGen/TectonicPlateTests.java
@@ -15,39 +15,52 @@ import jdz.RTGen.dataType.TectonicPlate;
 public class TectonicPlateTests {
 
 	private Map map;
-	private TectonicPlate plate;
+	private TectonicPlate plate, plate2;
 
 	@Before
 	public void setup() {
 		map = new Map(4, 1);
 	}
-	
+
 	@Test
 	public void testIndex() {
 		map = new Map(2, 2);
 		plate = makePlate(map, 0, 0, 0, 0);
-		
+
 		// general
 		assertEquals(0, plate.cellIndex(0, 0));
 		assertEquals(1, plate.cellIndex(1, 0));
 		assertEquals(2, plate.cellIndex(0, 1));
 		assertEquals(3, plate.cellIndex(1, 1));
-		
+
 		// wrap left
 		assertEquals(1, plate.cellIndex(-1, 0));
 		assertEquals(3, plate.cellIndex(-1, 1));
-		
+
 		// warp right
 		assertEquals(0, plate.cellIndex(2, 0));
 		assertEquals(2, plate.cellIndex(2, 1));
-		
+
 		// wrap top
 		assertEquals(0, plate.cellIndex(1, -1));
 		assertEquals(1, plate.cellIndex(0, -1));
-		
+
 		// wrap bottom
 		assertEquals(3, plate.cellIndex(0, 2));
 		assertEquals(2, plate.cellIndex(1, 2));
+	}
+
+	@Test
+	public void testOverlap() {
+		plate = makePlate(map, 0, 1, 1, 0);
+		plate2 = makePlate(map, 1, 1, 0, 0);
+		assertArrayEquals(new int[]{0, 1, 0, 0}, toInt(plate.getMasksOverlap(plate2)));
+	}
+	
+	@Test
+	public void testInvert() {
+		plate = makePlate(map, 0, 1, 1, 0);
+		assertArrayEquals(new int[]{1, 0, 0, 1}, toInt(plate.getInvertedMask()));
 	}
 
 	@Test
@@ -60,9 +73,9 @@ public class TectonicPlateTests {
 
 	@Test
 	public void testMoveVerticalWrap() {
-		map = new Map(2, 2);
-		plate = makePlate(map, 1, 0, 0, 1);
-		int[] expected = new int[] { 0, 1, 1, 0 };
+		map = new Map(4, 2);
+		plate = makePlate(map, 0, 0, 0, 0, 0, 1, 0, 1);
+		int[] expected = new int[] { 0, 1, 0, 1, 1, 0, 1, 0 };
 		plate.velocity = new Point2D(0, -1);
 		assertPlateEquals(expected, plate.step());
 	}
