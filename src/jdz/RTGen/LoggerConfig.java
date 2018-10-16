35e9945cffba06361e587fc09f062482ccb845a1
bakerjaid <jaidencolebaker@gmail.com>
2018-10-16 15:26:24 +1300

    Added logger configuration for less verbose messages

---
+++ src/jdz/RTGen/LoggerConfig.java
@@ -0,0 +1,28 @@
+
+package jdz.RTGen;
+
+import java.util.Date;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class LoggerConfig {
+	public static void init() {
+		Logger logger = Logger.getGlobal();
+		logger.setUseParentHandlers(false);
+
+		ConsoleHandler handler = new ConsoleHandler();
+		handler.setFormatter(new SimpleFormatter() {
+			private static final String format = "[%1$tT] [%2$-7s] %3$s %n";
+
+			@Override
+			public synchronized String format(LogRecord lr) {
+				return String.format(format, new Date(lr.getMillis()), lr.getLevel().getLocalizedName(),
+						lr.getMessage());
+			}
+		});
+		logger.addHandler(handler);
+	}
+
+}
