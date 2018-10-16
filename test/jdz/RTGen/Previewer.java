26789865835d32d3a9fc869ff3144b552077da82
bakerjaid <jaidencolebaker@gmail.com>
2018-10-17 11:36:38 +1300

    Cleaned up Previewer

--- test/jdz/RTGen/Previewer.java
+++ test/jdz/RTGen/Previewer.java
@@ -3,24 +3,27 @@ package jdz.RTGen;
 
 import java.awt.FlowLayout;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
 import jdz.RTGen.dataType.Map;
 
-public abstract class Previewer implements ActionListener {
-	private static final int PREVIEW_WIDTH = 512, PREVIEW_HEIGHT = 256;
+public abstract class Previewer {
+	private static final int PREVIEW_WIDTH = 1024, PREVIEW_HEIGHT = 512;
 
 	protected Map map;
 
 	private JFrame frame;
 	private ImageIcon icon;
+
+	private BufferedImage lastUnscaledImage;
 
 	public Previewer(int mapSize) {
 		this(mapSize, -1);
@@ -32,32 +35,46 @@ public abstract class Previewer implements ActionListener {
 		LoggerConfig.init();
 		init();
 
-		icon = new ImageIcon(getScaledImage());
-
-		if (updateMS > 0)
-			new Timer(updateMS, this).start();
-
 		frame = new JFrame();
 		frame.getContentPane().setLayout(new FlowLayout());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.setSize(PREVIEW_WIDTH, PREVIEW_HEIGHT);
+
+		lastUnscaledImage = createPreview();
+		icon = new ImageIcon();
+		redrawImage();
 
 		frame.getContentPane().add(new JLabel(icon));
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
-
+		frame.setSize(PREVIEW_WIDTH, PREVIEW_HEIGHT);
 		frame.setVisible(true);
+
+		if (updateMS > 0)
+			new Timer(updateMS, (e) -> {
+				lastUnscaledImage = createPreview();
+				redrawImage();
+			}).start();
+
+		frame.addComponentListener(new ComponentAdapter() {
+			public void componentResized(ComponentEvent e) {
+				redrawImage();
+			}
+		});
+
+		frame.setResizable(false);
 	}
 
 	protected void init() {}
 
-	private Image getScaledImage() {
-		return createPreview().getScaledInstance(PREVIEW_WIDTH, PREVIEW_HEIGHT, Image.SCALE_SMOOTH);
-	}
-
 	public abstract BufferedImage createPreview();
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		icon.setImage(getScaledImage());
-		frame.repaint();
+	public void redrawImage() {
+		SwingUtilities.invokeLater(() -> {
+			Image scaledImage = lastUnscaledImage.getScaledInstance(frame.getWidth(), frame.getHeight(),
+					Image.SCALE_SMOOTH);
+			icon.setImage(scaledImage);
+			frame.repaint();
+		});
 	}
 }
