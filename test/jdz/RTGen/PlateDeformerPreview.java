8f873b685329611665006dfe30123c30c332e460
bakerjaid <jaidencolebaker@gmail.com>
2018-10-18 16:11:52 +1300

    Minor preview changes

--- test/jdz/RTGen/PlateDeformerPreview.java
+++ test/jdz/RTGen/PlateDeformerPreview.java
@@ -3,6 +3,8 @@ package jdz.RTGen;
 
 import java.awt.image.BufferedImage;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import jdz.RTGen.algorithms.plateGeneration.TectonicPlateGenerator;
 import jdz.RTGen.algorithms.tectonics.TectonicPlateDeformer;
@@ -14,7 +16,7 @@ import jdz.RTGen.renderers.PlateListRenderer;
 public class PlateDeformerPreview extends Previewer {
 	private static final int MAP_SIZE = 256;
 	private static final int NUM_PLATES = 24;
-	private static final int MS_PER_STEP = 1000;
+	private static final int MS_PER_STEP = 100;
 
 	public static void main(String[] args) {
 		new PlateDeformerPreview();
@@ -26,6 +28,7 @@ public class PlateDeformerPreview extends Previewer {
 
 	@Override
 	public void init() {
+		Logger.getGlobal().setLevel(Level.SEVERE);
 		map = new Map(map.width, map.height, 100);
 		List<TectonicPlate> plates = TectonicPlateGenerator.getRandom().generatePlates(map, NUM_PLATES);
 		map.setPlates(plates);
