a9659c83c7898ce5ddc1e5e00f9573fa71b196fb
bakerjaid <jaidencolebaker@gmail.com>
2018-10-22 17:13:30 +1300

    Improved brightness and contrast

--- src/jdz/RTGen/renderers/HeightMapRenderer.java
+++ src/jdz/RTGen/renderers/HeightMapRenderer.java
@@ -4,6 +4,7 @@ package jdz.RTGen.renderers;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 
+import jdz.RTGen.dataType.Biome;
 import jdz.RTGen.dataType.Map;
 
 public class HeightMapRenderer extends Renderer {
@@ -15,19 +16,25 @@ public class HeightMapRenderer extends Renderer {
 
 	@Override
 	public void render(BufferedImage image, Map map) {
-		float minHeight = map.getMinHeight();
-		float maxHeight = map.getMaxHeight();
+		float minLandHeight = map.getSeaLevel();
+		float maxLandHeight = map.getMaxHeight();
+
+		float minOceanHeight = map.getMinHeight();
+		float maxOceanHeight = map.getSeaLevel();
 
 		for (int x = 0; x < map.width; x++)
 			for (int y = 0; y < map.height; y++) {
 				float height = map.getHeight(x, y);
-				float ratio = (height - minHeight) / (maxHeight - minHeight);
+				float ratio = map.getBiome(x, y) == Biome.OCEAN
+						? (height - minOceanHeight) / (maxOceanHeight - minOceanHeight)
+						: (height - minLandHeight) / (maxLandHeight - minLandHeight);
 				image.setRGB(x, y, multiplyValue(image.getRGB(x, y), ratio));
 			}
 	}
 
 	private int multiplyValue(int rgb, float ratio) {
 		Color c = new Color(rgb);
+		ratio = ratio * 0.5f + 0.5f;
 		float[] hsv = Color.RGBtoHSB(c.getRed(), c.getGreen(), c.getBlue(), null);
 		return Color.HSBtoRGB(hsv[0], hsv[1], hsv[2] * ratio);
 	}
