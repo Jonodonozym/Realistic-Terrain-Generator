4227d096f615cf04b5708f95aec4de4d0a68740d
bakerjaid <jaidencolebaker@gmail.com>
2018-10-23 10:20:36 +1300

    removed level from logging

--- src/jdz/RTGen/LoggerConfig.java
+++ src/jdz/RTGen/LoggerConfig.java
@@ -19,12 +19,11 @@ public class LoggerConfig {
 
 	public static SimpleFormatter getFormatter() {
 		return new SimpleFormatter() {
-			private static final String format = "[%1$tT] [%2$-7s] %3$s %n";
+			private static final String format = "[%1$tT] %2$s %n";
 
 			@Override
 			public synchronized String format(LogRecord lr) {
-				return String.format(format, new Date(lr.getMillis()), lr.getLevel().getLocalizedName(),
-						lr.getMessage());
+				return String.format(format, new Date(lr.getMillis()), lr.getMessage());
 			}
 		};
 	}
